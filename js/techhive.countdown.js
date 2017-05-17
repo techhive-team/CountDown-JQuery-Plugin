7e4c08a4984299a942c9ba0515b5cab7db780a00
sagar32 <chopada.sagar32@gmail.com>
2017-05-17 15:00:56 +0530

    change UI

--- js/techhive.countdown.js
+++ js/techhive.countdown.js
@@ -1,52 +1,46 @@
-(function($){
-	
-	$.fn.techhiveCountDownInit=function(){
+(function ($) {
+
+	$.fn.techhiveCountDownInit = function () {
 		console.log(this);
-		var htmlContent='<div class="dash weeks_dash">'+
-                '<span class="dash_title">weeks</span>'+
-                '<div class="digit">0</div>'+
-                '<div class="digit">0</div>'+
-            '</div>'+
+		var htmlContent = '<div class="dash weeks_dash">' +
+			'<span class="dash_title">weeks</span>' +
+			'<div class="animated digit">0</div>' +
+			'</div>' +
 
-            '<div class="dash days_dash">'+
-                '<span class="dash_title">days</span>'+
-                '<div class="digit">0</div>'+
-                '<div class="digit">0</div>'+
-            '</div>'+
+			'<div class="dash days_dash">' +
+			'<span class="dash_title">days</span>' +
+			'<div class="animated digit">0</div>' +
+			'</div>' +
 
-            '<div class="dash hours_dash">'+
-                '<span class="dash_title">hours</span>'+
-                '<div class="digit">0</div>'+
-                '<div class="digit">0</div>'+
-            '</div>'+
+			'<div class="dash hours_dash">' +
+			'<span class="dash_title">hours</span>' +
+			'<div class="animated digit">0</div>' +
+			'</div>' +
 
-            '<div class="dash minutes_dash">'+
-                '<span class="dash_title">minutes</span>'+
-                '<div class="digit">0</div>'+
-                '<div class="digit">0</div>'+
-            '</div>'+
+			'<div class="dash minutes_dash">' +
+			'<span class="dash_title">minutes</span>' +
+			'<div class="animated digit">0</div>' +
+			'</div>' +
 
-            '<div class="dash seconds_dash">'+
-                '<span class="dash_title">seconds</span>'+
-                '<div class="digit">0</div>'+
-                '<div class="digit">0</div>'+
-            '</div>';
-		for (var i=0; i<this.length; i++)
-		  {
-			  var selector=$(this[i]),
-			  date=selector.attr('tech-Date');
-			  selector.html(htmlContent);
-				this.techhiveCountDown(date,selector);
-		  }
+			'<div class="dash seconds_dash">' +
+			'<span class="dash_title">seconds</span>' +
+			'<div class="animated digit">0</div>' +
+			'</div>';
+		for (var i = 0; i < this.length; i++) {
+			var selector = $(this[i]),
+				date = selector.attr('tech-Date');
+			selector.html(htmlContent);
+			this.techhiveCountDown(date, selector);
+		}
 	}
-	$.fn.techhiveCountDown = function (toDate,targetSelector) {
+	$.fn.techhiveCountDown = function (toDate, targetSelector) {
 
 
 		diffSecs = this.setCountDown(toDate);
 		// targetSelector=this;
 		console.log(targetSelector);
-		targetIdName=$(this).attr('class');
-		$(targetSelector).find('.digit').html('<div class="top"></div><div class="bottom"></div>');
+		targetIdName = $(this).attr('class');
+		$(targetSelector).find('.digit').html('<div class="top animated"></div><div class="bottom animated"></div>');
 		$(this).doCountDown(targetSelector, diffSecs, 500);
 
 		return this;
@@ -54,17 +48,16 @@
 	};
 
 	$.fn.setCountDown = function (toDate) {
-		
 
-		if (toDate)
-		{
+
+		if (toDate) {
 			targetTime = new Date(toDate);
 		}
-		
+
 
 		var nowTime = new Date();
 
-		diffSecs = Math.floor((targetTime.valueOf()-nowTime.valueOf())/1000);
+		diffSecs = Math.floor((targetTime.valueOf() - nowTime.valueOf()) / 1000);
 
 		$.data(this[0], 'diffSecs', diffSecs);
 
@@ -72,28 +65,26 @@
 	};
 
 	$.fn.doCountDown = function (id, diffSecs, duration) {
+
+
 		$this = id;
-		if (diffSecs <= 0)
-		{
+		if (diffSecs <= 0) {
 			diffSecs = 0;
-			if ($.data($this[0], 'timer'))
-			{
+			if ($.data($this[0], 'timer')) {
 				clearTimeout($.data($this[0], 'timer'));
 			}
 		}
 
 		secs = diffSecs % 60;
-		mins = Math.floor(diffSecs/60)%60;
-		hours = Math.floor(diffSecs/60/60)%24;
-		if ($.data($this[0], 'omitWeeks') == true)
-		{
-			days = Math.floor(diffSecs/60/60/24);
-			weeks = Math.floor(diffSecs/60/60/24/7);
+		mins = Math.floor(diffSecs / 60) % 60;
+		hours = Math.floor(diffSecs / 60 / 60) % 24;
+		if ($.data($this[0], 'omitWeeks') == true) {
+			days = Math.floor(diffSecs / 60 / 60 / 24);
+			weeks = Math.floor(diffSecs / 60 / 60 / 24 / 7);
 		}
-		else 
-		{
-			days = Math.floor(diffSecs/60/60/24)%7;
-			weeks = Math.floor(diffSecs/60/60/24/7);
+		else {
+			days = Math.floor(diffSecs / 60 / 60 / 24) % 7;
+			weeks = Math.floor(diffSecs / 60 / 60 / 24 / 7);
 		}
 
 		$this.dashChangeTo(id, 'seconds_dash', secs, duration ? duration : 800);
@@ -103,49 +94,37 @@
 		$this.dashChangeTo(id, 'weeks_dash', weeks, duration ? duration : 1200);
 
 		$.data($this[0], 'diffSecs', diffSecs);
-		if (diffSecs > 0)
-		{
+		if (diffSecs > 0) {
 			e = $this;
-			t = setTimeout(function() { e.doCountDown(id, diffSecs-1) } , 1000);
+			t = setTimeout(function () {
+				e.doCountDown(id, diffSecs - 1)
+			}, 1000);
 			$.data(e[0], 'timer', t);
-		} 
-		else if (cb = $.data($this[0], 'callback')) 
-		{
+		}
+		else if (cb = $.data($this[0], 'callback')) {
 			$.data($this[0], 'callback')();
 		}
 
 	};
 
-	$.fn.dashChangeTo = function(id, dash, n, duration) {
-		  $this = id;
-		 
-		  for (var i=($this.find('.' + dash + ' .digit').length-1); i>=0; i--)
-		  {
-				var d = n%10;
-				n = (n - d) / 10;
-				$this.digitChangeTo($this.find('.'+dash).find(' .digit:eq('+i+')'), d, duration);
-		  }
-	};
-
-	$.fn.digitChangeTo = function (digit, n, duration) {
-		if (!duration)
-		{
-			duration = 800;
+	$.fn.dashChangeTo = function (id, dash, n, duration) {
+		$this = id;
+		n = n < 10 ? "0" + n : n;
+		console.log($('.' + dash).find(' .digit').html()!= n + "")
+		if ($('.' + dash).find(' .digit').html() != n + "") {
+			$this.find('.' + dash).find(' .digit').html(n).addClass('flipInY').removeClass("flipOutY");
+			setTimeout(function () {
+				console.log("timeout");
+				$this.find('.' + dash).find(' .digit').addClass("flipOutY").removeClass('flipInY');
+			}, duration);
+		} else {
+			$('.' + dash).find(' .digit').html(n).removeClass('flipOutY flipInY');
 		}
-		divTop=digit.find(' div.top');
-		divBottom=digit.find(' div.bottom');
-		if (divTop.html() != n + '')
-		{
-
-			divTop.css({'display': 'none'});
-			divTop.html((n ? n : '0')).slideDown(duration);
-
-			divBottom.animate({'height': ''}, duration, function() {
-				divBottom.html(divTop.html());
-				divBottom.css({'display': 'block', 'height': ''});
-				divTop.hide().slideUp(10);
-			});
-		}
+		// for (var i = ($this.find('.' + dash + ' .digit').length - 1); i >= 0; i--) {
+		// 	var d = n % 10;
+		// 	n = (n - d) / 10;
+		// 	$this.digitChangeTo($this.find('.' + dash).find(' .digit:eq(' + i + ')'), d, duration);
+		// }
 	};
 
 })(jQuery);
